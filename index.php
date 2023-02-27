089a70259046f9d1adcfe8eb4e5e86edcdb8c2c5
GoLuDev <ketroma@yahoo.com>
2023-02-27 22:13:58 +0300

    Changed name for $

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$b = \Client\API\ConnectingToService::getInstance();
-$a = $b->connectToService('books?_', 1);
+$connect = \Client\API\ConnectingToService::getInstance();
+$result = $connect->connectToService('books?_', 1);
 $save = new \Client\API\SaveResultToFile();
-$save->saveResult($a);
+$save->saveResult($result);
